f22744714f6c5ecc207f14af1329d70ba806258f
giang <giang@tenpoint7.com>
2025-03-04 05:09:33 +0700

    update ALLOWED_HOSTS

--- todo_project/settings.py
+++ todo_project/settings.py
@@ -12,7 +12,7 @@ SECRET_KEY = 'django-insecure-your-secret-key-here'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['to-do-list-django-7rbpkqwep-gianghks-projects.vercel.app']
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 INSTALLED_APPS = [
