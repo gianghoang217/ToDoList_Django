33c67e4b19806bc2405e50bbf5ee72684a64eef3
giang <giang@tenpoint7.com>
2025-03-17 09:28:46 +0700

    1-create register function for react

--- todo_api/views.py
+++ todo_api/views.py
@@ -9,6 +9,37 @@ from .models import Todo
 from .serializers import TodoSerializer
 from django.views.decorators.csrf import csrf_exempt
 from django.utils.decorators import method_decorator
+from django.contrib.auth.models import User
+
+
+@method_decorator(csrf_exempt, name='dispatch')
+class RegisterView(APIView):
+    permission_classes = []
+    def post(self, request):
+        username = request.data.get('username')
+        password = request.data.get('password')
+        
+        if not username or not password:
+            return Response(
+                {'error': 'Both username and password are required'},
+                status=status.HTTP_400_BAD_REQUEST
+            )
+            
+        if User.objects.filter(username=username).exists():
+            return Response(
+                {'error': 'Username already exists'},
+                status=status.HTTP_400_BAD_REQUEST
+            )
+            
+        user = User.objects.create_user(username=username, password=password)
+        token, created = Token.objects.get_or_create(user=user)
+        
+        
+        return Response({
+            'token': token.key,
+            'user_id': user.pk,
+            'username': user.username
+        }, status=status.HTTP_201_CREATED)
 
 
 @method_decorator(csrf_exempt, name='dispatch')
